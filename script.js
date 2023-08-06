da25beb75da4b4224cfc78e9c7dcc551853c3adf
Daniel Barton <llama552621@gmail.com>
2023-08-06 14:59:21 +1000

    commit

--- script.js
+++ script.js
@@ -1,27 +1,5 @@
+console.log("Hello there! :)")
 
-async function getGa() {
-  const response = await fetch("https://api.daniel4scratch.repl.co/ga?id=main");
-  const data = await response.text();
-  return data;
-}
-
-async function start() {
-    const ga = await getGa()
-    if (ga != ""){
-        
-        document.getElementById("ga").style.display = "block"
-        document.getElementById("ga").innerHTML += ga
-}
-}
-
-start()
-
-var element = document.getElementById("left");
-    var width = element.offsetWidth;
-    document.getElementById("ga").style.maxWidth = width - 20 + "px";
-
-window.addEventListener("resize", function() {
-    var element = document.getElementById("left");
-    var width = element.offsetWidth;
-    document.getElementById("ga").style.maxWidth = width - 20 + "px";
-  });
+document.querySelector(".navbar").innerHTML = `<a href="index.html"><span class="material-symbols-rounded">home</span><p>Home</p></a>
+      <a href="projects.html"><i class="ri-git-repository-fill"></i><p>Projects</p></a>
+      <a href="about.html"><i class="ri-user-fill"></i><p>About</p></a>`
