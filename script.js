0ea16db73bbda990b0a3c3407cb0927ca32d9762
Daniel Barton <llama552621@gmail.com>
2023-08-06 17:18:12 +1000

    its done

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 console.log("Hello there! :)")
 
-document.querySelector(".navbar").innerHTML = `<a href="index.html"><span class="material-symbols-rounded">home</span><p>Home</p></a>
+document.querySelector(".navbar").innerHTML = `<a href="index.html"><i class="ri-home-2-fill"></i><p>Home</p></a>
       <a href="projects.html"><i class="ri-git-repository-fill"></i><p>Projects</p></a>
       <a href="about.html"><i class="ri-user-fill"></i><p>About</p></a>`
+
+      let keylog = [];
+      document.onkeydown = function (e) {
+          let keypressed = e.key;
+          keylog.push(keypressed);
+          if ("necoarc".startsWith(keylog.join(""))) {
+              if(keylog.join("") == "necoarc"){
+                  window.location.href = "necoarc.html";
+              }
+          }else{
+            keylog=[]
+        }
+      };
