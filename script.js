e7a05a873c56d163d5b8d9e875999f2242954af9
Daniel4-Scratch <daniel4scratchhelp@gmail.com>
2024-07-28 09:26:14 +1000

    file icons

--- script.js
+++ script.js
@@ -4,15 +4,19 @@ document.querySelector(".navbar").innerHTML = `<a href="index.html"><i class="ri
       <a href="projects.html"><i class="ri-git-repository-fill"></i><p>Projects</p></a>
       <a href="about.html"><i class="ri-user-fill"></i><p>About</p></a>`
 
-      let keylog = [];
-      document.onkeydown = function (e) {
-          let keypressed = e.key;
-          keylog.push(keypressed);
-          if ("necoarc".startsWith(keylog.join(""))) {
-              if(keylog.join("") == "necoarc"){
-                  window.location.href = "necoarc.html";
-              }
-          }else{
-            keylog=[]
+let keylog = [];
+document.onkeydown = function (e) {
+    let keypressed = e.key;
+    keylog.push(keypressed);
+    if ("necoarc".startsWith(keylog.join(""))) {
+        if (keylog.join("") == "necoarc") {
+            window.location.href = "necoarc.html";
         }
-      };
+    } else {
+        keylog = []
+    }
+};
+
+document.getElementById("pdf").onclick = function () {
+    open("documents/Daniel's Projects and Skills.pdf")
+}
